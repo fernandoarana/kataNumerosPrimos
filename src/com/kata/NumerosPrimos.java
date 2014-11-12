f5d7b745e2a65a2d8223da14e9064576dcfaed50
fernandoarana <fernando.arana@tralix.com>
2014-11-11 19:40:44 -0600

    Redo

--- src/com/kata/NumerosPrimos.java
+++ src/com/kata/NumerosPrimos.java
@@ -1,15 +1,7 @@
 package com.kata;
 
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class NumerosPrimos {
 
-    public List<Integer> obtenerNumerosPrimos(final int i) {
-        List<Integer> list = new ArrayList<Integer>();
-        list.add(i);
-        return list;
-    }
 
 }
--- test/com/kata/NumerosPrimosTest.java
+++ test/com/kata/NumerosPrimosTest.java
@@ -1,11 +1,5 @@
 package com.kata;
-import static org.junit.Assert.*;
-
-import java.util.List;
-
 import org.junit.Before;
-import org.junit.Test;
-
 import com.kata.NumerosPrimos;
 
 
@@ -16,12 +10,5 @@ public class NumerosPrimosTest {
     @Before
     public void setUp() throws Exception {
         numerosPrimos = new NumerosPrimos();
-    }
-
-    @Test
-    public void dadoUnNumeroPrimoSinMultiplosRegresaElMismo() {
-        List<Integer> primos = numerosPrimos.obtenerNumerosPrimos(2);
-        assertEquals(primos.size(), 1);
-        assertEquals(primos.get(0).intValue(), 2);
     }
 }
