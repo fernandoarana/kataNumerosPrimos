13c9c783c02438e3fc58e6f9baac157cc4a7211c
fernandoarana <fernando.arana@tralix.com>
2014-11-11 19:34:40 -0600

    se pasa el primer escenario (verde)

--- src/com/kata/NumerosPrimos.java
+++ src/com/kata/NumerosPrimos.java
@@ -1,12 +1,15 @@
 package com.kata;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class NumerosPrimos {
 
     public List<Integer> obtenerNumerosPrimos(final int i) {
-        return null;
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(i);
+        return list;
     }
 
 }
